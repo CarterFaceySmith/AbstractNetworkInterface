b145f223660c327d1d615e59af9b4be33b91d32e
Carter <carterfs@proton.me>
2024-09-18 10:26:44 +1000

    chore: Remove FIXME re segfault bug

--- AbstractNetworkInterfaceTest.cpp
+++ AbstractNetworkInterfaceTest.cpp
@@ -104,7 +104,6 @@ TEST_F(NetworkImplementationTest, SendReceiveComplexBlob) {
     std::cout << "Complex blob sent" << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds(100)); // Allow time for message to be sent
 
-    // FIXME: Causes segmentation fault
     std::cout << "Receiving complex blob" << std::endl;
 
     try {
